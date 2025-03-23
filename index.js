54dad93679b6773690ed0f32f763ad39e6d90e4f
jairo jean <jairojean.br@gmail.com>
2025-03-23 13:43:17 -0300

    feat: fix files

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
-
-// Middleware para servir arquivos estáticos
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota para gerar o áudio do texto
+ 
 app.get('/audio', async (req, res) => {
     const texto = req.query.texto;
     
-    // Gera a URL do áudio
+ 
     const urlAudio = googleTTS.getAudioUrl(texto, {
         lang: 'pt',
         slow: false,
@@ -22,10 +21,10 @@ app.get('/audio', async (req, res) => {
     });
 
     try {
-        // Requisição para obter o áudio diretamente do Google TTS
+  
         const audioResponse = await axios.get(urlAudio, { responseType: 'arraybuffer' });
         
-        // Define o cabeçalho correto para retorno de áudio
+      
         res.set('Content-Type', 'audio/mpeg');
         res.send(audioResponse.data);
     } catch (error) {
@@ -33,13 +32,12 @@ app.get('/audio', async (req, res) => {
         res.status(500).send('Erro ao gerar áudio');
     }
 });
-
-// Rota para servir o HTML
+ 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Iniciar o servidor
+ 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
